35ef7d31e728f743607817acc9e7cd7b4c3763f8
James Shieh <jamesshieh0510@gmail.com>
2022-07-04 21:34:11 +0800

    add env.sh example

---
+++ ubuntu/docker-registry/env.sh
@@ -0,0 +1,7 @@
+# User Information for Docker Registry
+username="xxx"
+passwd="xxxx"
+server_name="domian.com"
+docker_registry_port="5000"
+docker_registry_web_prot="80"
+docker_registry_ip="140.116.123.123"
